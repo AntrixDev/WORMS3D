7b5113bd2e9d50b27ea260d786d0c1cd2a7706b5
AntrixDev <x.aniutka@gmail.com>
2026-04-28 15:13:52 +0200

    add helper function for vertices and creating all 36 vertices for the cube

--- src/main.ts
+++ src/main.ts
@@ -23,3 +23,44 @@ const Camera = d.struct({
 const Transform = d.struct({
   model: d.mat4x4f, //identity, translate, rotate, scale
 });
+
+function createFace(
+  vertices: number[][], //list of vertices for 1 side of cube,
+  color: d.Infer<typeof Vertex>["color"]//color (all points on 1 side same clr)
+): d.Infer<typeof Vertex>[] {
+  return vertices.map((pos) => ({
+    position: d.vec4f(...(pos as [number, number, number, number])),
+    color,
+  }));
+}
+
+//all of the traingles have to be drawn ccw or cw
+//the starting vertice doesn't matter as long as the direction of drawing is right
+
+function createCube(): d.Infer<typeof Vertex>[] { //x y z w(always =1)
+  const front = createFace([ 
+    [-1, -1,  1, 1], [1, -1,  1, 1], [1,  1,  1, 1], //x y z w 
+    [-1, -1,  1, 1], [1,  1,  1, 1], [-1,  1,  1, 1], //x y z w
+  ], d.vec4f(1, 0, 0, 1));
+  const back = createFace([
+    [-1, -1, -1, 1], [-1,  1, -1, 1], [1, -1, -1, 1],
+    [ 1, -1, -1, 1], [-1,  1, -1, 1], [1,  1, -1, 1],
+  ], d.vec4f(0, 1, 0, 1));
+  const top = createFace([
+    [-1, 1, -1, 1], [-1, 1,  1, 1], [1, 1, -1, 1],
+    [ 1, 1, -1, 1], [-1, 1,  1, 1], [1, 1,  1, 1],
+  ], d.vec4f(0, 0, 1, 1));
+  const bottom = createFace([
+    [-1, -1, -1, 1], [ 1, -1, -1, 1], [-1, -1,  1, 1],
+    [ 1, -1, -1, 1], [ 1, -1,  1, 1], [-1, -1,  1, 1],
+  ], d.vec4f(1, 1, 0, 1));
+  const right = createFace([
+    [1, -1, -1, 1], [1,  1, -1, 1], [1, -1,  1, 1],
+    [1, -1,  1, 1], [1,  1, -1, 1], [1,  1,  1, 1],
+  ], d.vec4f(1, 0, 1, 1));
+  const left = createFace([
+    [-1, -1, -1, 1], [-1, -1,  1, 1], [-1,  1, -1, 1],
+    [-1, -1,  1, 1], [-1,  1,  1, 1], [-1,  1, -1, 1],
+  ], d.vec4f(0, 1, 1, 1));
+  return [...front, ...back, ...top, ...bottom, ...right, ...left]; // returns flat array 36 vertices
+}
