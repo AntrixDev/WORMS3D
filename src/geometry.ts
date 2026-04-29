2d5b9aff66e856a1d716a0e70558b3188b75d1ca
AntrixDev <x.aniutka@gmail.com>
2026-04-29 16:20:45 +0200

    mod moved cube creating to seperate file

---
+++ src/geometry.ts
@@ -0,0 +1,49 @@
+import tgpu, { d} from "typegpu";
+
+const Vertex = d.struct({
+  position: d.vec4f,
+  color: d.vec4f,
+});
+
+
+export const vertexLayout = tgpu.vertexLayout(d.arrayOf(Vertex));
+
+function createFace(
+  vertices: number[][],
+  color: d.Infer<typeof Vertex>["color"]
+): d.Infer<typeof Vertex>[] {
+  return vertices.map((pos) => ({
+    position: d.vec4f(...(pos as [number, number, number, number])),
+    color,
+  }));
+}
+
+
+export function createCube(): d.Infer<typeof Vertex>[] {
+  const front = createFace([
+    [-1, -1,  1, 1], [1, -1,  1, 1], [1,  1,  1, 1],
+    [-1, -1,  1, 1], [1,  1,  1, 1], [-1,  1,  1, 1],
+  ], d.vec4f(1, 0, 0, 1));
+  const back = createFace([
+    [-1, -1, -1, 1], [-1,  1, -1, 1], [1, -1, -1, 1],
+    [ 1, -1, -1, 1], [-1,  1, -1, 1], [1,  1, -1, 1],
+  ], d.vec4f(0, 1, 0, 1));
+  const top = createFace([
+    [-1, 1, -1, 1], [-1, 1,  1, 1], [1, 1, -1, 1],
+    [ 1, 1, -1, 1], [-1, 1,  1, 1], [1, 1,  1, 1],
+  ], d.vec4f(0, 0, 1, 1));
+  const bottom = createFace([
+    [-1, -1, -1, 1], [ 1, -1, -1, 1], [-1, -1,  1, 1],
+    [ 1, -1, -1, 1], [ 1, -1,  1, 1], [-1, -1,  1, 1],
+  ], d.vec4f(1, 1, 0, 1));
+  const right = createFace([
+    [1, -1, -1, 1], [1,  1, -1, 1], [1, -1,  1, 1],
+    [1, -1,  1, 1], [1,  1, -1, 1], [1,  1,  1, 1],
+  ], d.vec4f(1, 0, 1, 1));
+  const left = createFace([
+    [-1, -1, -1, 1], [-1, -1,  1, 1], [-1,  1, -1, 1],
+    [-1, -1,  1, 1], [-1,  1,  1, 1], [-1,  1, -1, 1],
+  ], d.vec4f(0, 1, 1, 1));
+  return [...front, ...back, ...top, ...bottom, ...right, ...left];
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import tgpu, { d, std } from "typegpu";
 import * as m from "wgpu-matrix";
 import { Camera, createCamera } from "./camera";
+import { vertexLayout, createCube } from "./geometry";
 
 const root = await tgpu.init();
 
@@ -18,56 +19,11 @@ const context = root.configureContext({ canvas, alphaMode: "premultiplied" });
 const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
 
 
-const Vertex = d.struct({
-  position: d.vec4f,
-  color: d.vec4f,
-});
-
 const Transform = d.struct({
   model: d.mat4x4f,
 });
 
-function createFace(
-  vertices: number[][],
-  color: d.Infer<typeof Vertex>["color"]
-): d.Infer<typeof Vertex>[] {
-  return vertices.map((pos) => ({
-    position: d.vec4f(...(pos as [number, number, number, number])),
-    color,
-  }));
-}
-
-function createCube(): d.Infer<typeof Vertex>[] {
-  const front = createFace([
-    [-1, -1,  1, 1], [1, -1,  1, 1], [1,  1,  1, 1],
-    [-1, -1,  1, 1], [1,  1,  1, 1], [-1,  1,  1, 1],
-  ], d.vec4f(1, 0, 0, 1));
-  const back = createFace([
-    [-1, -1, -1, 1], [-1,  1, -1, 1], [1, -1, -1, 1],
-    [ 1, -1, -1, 1], [-1,  1, -1, 1], [1,  1, -1, 1],
-  ], d.vec4f(0, 1, 0, 1));
-  const top = createFace([
-    [-1, 1, -1, 1], [-1, 1,  1, 1], [1, 1, -1, 1],
-    [ 1, 1, -1, 1], [-1, 1,  1, 1], [1, 1,  1, 1],
-  ], d.vec4f(0, 0, 1, 1));
-  const bottom = createFace([
-    [-1, -1, -1, 1], [ 1, -1, -1, 1], [-1, -1,  1, 1],
-    [ 1, -1, -1, 1], [ 1, -1,  1, 1], [-1, -1,  1, 1],
-  ], d.vec4f(1, 1, 0, 1));
-  const right = createFace([
-    [1, -1, -1, 1], [1,  1, -1, 1], [1, -1,  1, 1],
-    [1, -1,  1, 1], [1,  1, -1, 1], [1,  1,  1, 1],
-  ], d.vec4f(1, 0, 1, 1));
-  const left = createFace([
-    [-1, -1, -1, 1], [-1, -1,  1, 1], [-1,  1, -1, 1],
-    [-1, -1,  1, 1], [-1,  1,  1, 1], [-1,  1, -1, 1],
-  ], d.vec4f(0, 1, 1, 1));
-  return [...front, ...back, ...top, ...bottom, ...right, ...left];
-}
-
 const cameraBuffer = createCamera(root, canvas);
-
-const vertexLayout = tgpu.vertexLayout(d.arrayOf(Vertex));
 
 const cubeBuffer = root
   .createBuffer(vertexLayout.schemaForCount(36), createCube())
