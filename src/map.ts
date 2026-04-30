23e4270034d0bf511b41b306e7a914a21337585d
AntrixDev <x.aniutka@gmail.com>
2026-04-30 12:03:57 +0200

    add logic for the array with position for all cubes

--- src/map.ts
+++ src/map.ts
@@ -1,1 +1,25 @@
-//cube 100x100 (110x10 tseting) position of 6 sides *layers
+import {d} from "typegpu";
+import * as m from "wgpu-matrix";
+
+ const cubeInstance = d.struct({
+    model: d.mat4x4f,
+});
+
+const plateSize = 10;
+
+const instanceArray: {
+    instanceAr: Float32Array 
+}[]=[];
+
+for(let i=0; i <plateSize*plateSize; i++){
+
+    const x = (i% plateSize) * 2 - plateSize;
+    const y =-2;
+    const z = Math.floor((i/plateSize)) * 2 - plateSize;
+
+    instanceArray.push({
+        instanceAr: m.mat4.translate([x, y ,z], d.mat4x4f()),
+    });
+}
+
+ const cubeCount = instanceArray.length;
