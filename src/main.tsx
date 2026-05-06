1740a6ed09b0043c2971754da68837301d46e8d2
AntrixDev <x.aniutka@gmail.com>
2026-05-06 11:13:28 +0200

    add navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import MainMenu from './ui/MainMenu'
-import PlayerMenu from './ui/PlayersMenu'
+import PlayersMenu from './ui/PlayersMenu'
+
+type Screen = 'main' | 'players'
+
+function App() {
+  const [screen, setScreen] = useState<Screen>('main')
+
+  return screen==='main' ? <MainMenu onPlay={() => setScreen('players')} /> : <PlayersMenu/>
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PlayerMenu />
+    <App />
   </StrictMode>,
 )
