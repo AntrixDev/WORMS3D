0d64eca336aae3825e8b492df56e4d84cd5fd04a
AntrixDev <x.aniutka@gmail.com>
2026-05-06 12:57:31 +0200

    add ui main menu design

--- src/ui/MainMenu.tsx
+++ src/ui/MainMenu.tsx
@@ -1,4 +1,7 @@
 import './MainMenu.css'
+import ceilingSlime from './assets/ceilingSlime.png'
+import explosion from './assets/explosion.png'
+import logo from './assets/logo.png'
 
 interface MainMenuProps {
   onPlay: () => void
@@ -6,9 +9,16 @@ interface MainMenuProps {
 
 function MainMenu({ onPlay }: MainMenuProps) {
   return (
-    <div className="main-menu">
-      <button className="play-btn" onClick={onPlay}>PLAY</button>
-    </div>
+    <section id='main-menu'>
+      <div className='flexCol'>
+        <img src={ceilingSlime} alt="Blue slime" id='ceilSlime' draggable='false'/>
+        <img src={explosion} alt="explosion" id='explosion' draggable='false'/>
+      </div>
+      <div className='flexCol' id='right'>
+        <img src={logo} alt="JELLYS 3D" id='logo' draggable='false'/>
+        <button className='play-btn' onClick={onPlay}>PLAY</button>
+      </div>
+    </section>
   )
 }
 
