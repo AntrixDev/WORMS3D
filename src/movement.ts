2eebed3f51439925a3b19bea1d6dfad27da77ba2
Annie Rączka <x.aniutka@gmail.com>
2026-05-12 23:53:06 +0200

    add 3x3x3 grid search

--- src/movement.ts
+++ src/movement.ts
@@ -17,3 +17,31 @@ function sdBox(p: number[], b: number[]): number {
 
   return outDist + inDist;
 }
+
+function getSceneSDF(px: number, py: number, pz: number): number { 
+  let minDist = Infinity;
+  const cx = Math.round(px);
+  const cy = Math.round(py);
+  const cz = Math.round(pz);
+
+  for (let ox = -1; ox <= 1; ox++) {
+      for (let oy = -1; oy <= 1; oy++) {
+        for (let oz = -1; oz <= 1; oz++) {
+          const bx = cx + ox;
+          const by = cy + oy;
+          const bz = cz + oz;
+
+          if (isSolidBlock(bx, by, bz)){
+              const localX = px - bx;
+              const localY = py - by;
+              const localZ = pz - bz;
+
+            
+              const dist = sdBox([localX, localY, localZ], [0.5, 0.5, 0.5]);
+              minDist = Math.min(minDist, dist);
+          }
+        }
+      }
+  }
+  return minDist;
+}
